9ca354a98598ef06243ea293fbc9e5cac46df994
Marcelo Junior <marcelo.jr63@gmail.com>
2020-07-07 18:57:37 -0300

    Reading commandos from stdin

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Ok")
+	for {
+		scanner := bufio.NewScanner(os.Stdin)
+		printPrompt()
+		if readNextLine(scanner) {
+			execComand(scanner.Text())
+		}
+	}
+}
+
+func printPrompt() {
+	fmt.Printf("anadb > ")
+}
+
+func readNextLine(scanner *bufio.Scanner) bool {
+	return scanner.Scan()
+}
+
+func execComand(command string) {
+	switch command {
+	case ".exit":
+		fmt.Println("Saindo :*")
+		os.Exit(0)
+	default:
+		fmt.Printf("Unrecognized command '%s'.\n", command)
+	}
 }
