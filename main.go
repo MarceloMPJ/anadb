967b7623773f7fbd07d7cd83088bc35aa2404f28
Marcelo Junior <marcelo.jr63@gmail.com>
2020-08-02 18:18:30 -0300

    Add functions executeStatement and prepareStatement

--- main.go
+++ main.go
@@ -6,12 +6,58 @@ import (
 	"os"
 )
 
+type metaCommandResult string
+
+const (
+	metaCommandSuccess             = "metaCommandSuccess"
+	metaCommandUnrecognizedCommand = "metaCommandUnrecognizedCommand"
+)
+
+type prepareCommandResult string
+
+const (
+	prepareSuccess               = "prepareSuccess"
+	prepareUnrecognizedStatement = "prepareUnrecognizedStatement"
+)
+
+type statementType string
+
+const (
+	statementInsert = "Insert"
+	statementSelect = "Select"
+)
+
+type statement struct {
+	statementType statementType
+}
+
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
 	for {
-		scanner := bufio.NewScanner(os.Stdin)
 		printPrompt()
-		if readNextLine(scanner) {
-			execComand(scanner.Text())
+		scanner.Scan()
+		input := scanner.Text()
+
+		statement := statement{}
+
+		if input[0] == '.' {
+			switch execComand(input) {
+			case metaCommandSuccess:
+				continue
+			case metaCommandUnrecognizedCommand:
+				fmt.Printf("Unrecognized command '%s'.\n", input)
+				continue
+			}
+		} else {
+			switch prepareStatement(input, &statement) {
+			case prepareSuccess:
+				executeStatement(&statement)
+				continue
+			case prepareUnrecognizedStatement:
+				fmt.Printf("Unrecognized keyword at start of '%s'.\n", input)
+				continue
+			}
 		}
 	}
 }
@@ -20,16 +66,37 @@ func printPrompt() {
 	fmt.Printf("anadb > ")
 }
 
-func readNextLine(scanner *bufio.Scanner) bool {
-	return scanner.Scan()
-}
-
-func execComand(command string) {
+func execComand(command string) metaCommandResult {
 	switch command {
 	case ".exit":
-		fmt.Println("Saindo :*")
+		fmt.Println("😘")
 		os.Exit(0)
 	default:
-		fmt.Printf("Unrecognized command '%s'.\n", command)
+		return metaCommandUnrecognizedCommand
+	}
+
+	return metaCommandSuccess
+}
+
+func prepareStatement(inputStatement string, statement *statement) prepareCommandResult {
+	if inputStatement[:6] == "insert" {
+		statement.statementType = statementInsert
+		return prepareSuccess
+	}
+
+	if inputStatement[:6] == "select" {
+		statement.statementType = statementSelect
+		return prepareSuccess
+	}
+
+	return prepareUnrecognizedStatement
+}
+
+func executeStatement(statement *statement) {
+	switch statement.statementType {
+	case statementInsert:
+		fmt.Println("Executando INSERT")
+	case statementSelect:
+		fmt.Println("Executando SELECT")
 	}
 }
